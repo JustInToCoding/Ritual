c2f9cdb86c72fd87334156cd115a56aa5cc8b882
PeterXhofleer <pm.xhofleer@gmail.com>
2016-01-30 00:32:44 +0100

    Startmenu toegevoegd

---
+++ Ritual/Assets/Resources/Scripts/Menu scripts/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+
+	//opens gameScene
+	public void StartGame(string sceneToChangeTo){
+		SceneManager.LoadScene(sceneToChangeTo);
+	}
+		
+}
