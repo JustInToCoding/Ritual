32bcfd21c08639e2a8a6a84ea1f8a8d094c8851c
PeterXhofleer <pm.xhofleer@gmail.com>
2016-01-29 23:43:23 +0100

    Player shoot toegevoegd

---
+++ Ritual/Assets/Resources/Scripts/Player scripts/BulletController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float direction;
+	public float speed;
+	public Animator anim;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start() {
+//		anim = GetComponent<Animator>();
+		rigidBody = GetComponent<Rigidbody2D>();
+//		gameObject.GetComponent<CircleCollider2D>().enabled = false;
+//		anim.Play("cannonball");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+//		float step = speed * Time.deltaTime;
+//		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		if(direction == 1){
+			rigidBody.velocity = new Vector3(0, speed, 0);
+		}else if(direction == 2){
+			rigidBody.velocity = new Vector3(-speed, 0, 0);
+		}else if(direction == 3){
+			rigidBody.velocity = new Vector3(0, -speed, 0);
+		}else if(direction == 4){
+			rigidBody.velocity = new Vector3(speed, 0, 0);
+		}
+
+//		if(Vector3.Distance(transform.position, target) < 0.1f){
+			//			print("anim explode");
+//			anim.CrossFade("cannonball_explosion", 0.4f);
+//			gameObject.GetComponent<CircleCollider2D>().enabled = true;
+//			StartCoroutine(removeObject());
+//		}
+	}
+
+	IEnumerator removeObject() {
+		yield return new WaitForSeconds(0.6f); //this will wait 1 second
+		Destroy(gameObject);
+	}
+}
---
+++ Ritual/Assets/Resources/Scripts/Player scripts/PlayerShoot.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public Rigidbody2D BulletPrefab;
+	public float BulletSpeed;
+	public float AttackSpeed;
+	float Cooldown;
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time >= Cooldown){
+
+			if(Input.GetKey(KeyCode.Space)){
+				Fire();
+			}
+			if (Input.GetMouseButton(0)) {
+				Fire();
+			}
+		}
+	}
+
+	void Fire(){
+
+			
+//		var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//			targetPos.z = transform.position.z;
+//
+//		print("y:"+Input.mousePosition+" x:"+targetPos.x+" z:"+targetPos.z);
+//			AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+//			audioSource.Play();
+
+		Rigidbody2D bPrefab = Instantiate(BulletPrefab, transform.position, Quaternion.identity) as Rigidbody2D;
+//			bPrefab.transform.position = Vector3.MoveTowards(transform.position, targetPos, BulletSpeed * Time.deltaTime);
+
+		//get the thing component on your instantiated object
+		BulletController cbController = bPrefab.GetComponent<BulletController>();
+		
+		PlayerController playerController = gameObject.GetComponent<PlayerController>();
+		//now get the variable from the script reference we just made
+		float playerDirection = playerController.direction;
+
+		cbController.direction = playerDirection;
+
+		Cooldown = Time.time + AttackSpeed;
+		//anim.Play("cannon");
+
+	}
+
+}
+
+
