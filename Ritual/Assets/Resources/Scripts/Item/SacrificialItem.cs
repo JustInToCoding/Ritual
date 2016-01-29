5cad42254c81860ee333c3a15ce58931e8657c8c
Romen Klaassen <Romen.Klaassen@hva.nl>
2016-01-30 00:12:21 +0100

    Added sacrificial Items for the ritual, added heldItems which tracks the player's held sacricial items, activates Ritual when 3 sacrificial items are held.

---
+++ Ritual/Assets/Resources/Scripts/Item/SacrificialItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SacrificialItem : Item {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	    player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            player.GetComponent<HeldItems>().heldSacrificialItems++;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Ritual/Assets/Resources/Scripts/Player scripts/HeldItems.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeldItems : MonoBehaviour {
+
+    public int currBoss;
+    public int heldSacrificialItems;
+    // public LevelGenerator level;
+
+	// Use this for initialization
+	void Start () {
+        currBoss = 1;
+        heldSacrificialItems = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (heldSacrificialItems >= 3)
+        {
+            StartRitual();
+            Reset();
+        }
+
+	}
+
+    void StartRitual()
+    {
+        // Start the ritual when 3 items are collected at the altar
+        // level.SpawnEnemies = true;
+        // GameObject Boss = Instantiate (Resources.Load("Resources/Prefabs/Bosses/Boss#1"), transform.position, transform.rotation);
+        // Reset();
+        
+    }
+
+    void Reset()
+    {
+        heldSacrificialItems = 0;
+    }
+}
