441c8ad83536b4b756f5e913acf8e94b11a201a0
Justin <justin@linku.nl>
2016-01-31 00:50:41 +0100

    EnemyManager v2

--- Ritual/Assets/Resources/Scripts/Player scripts/EnemyManager.cs
+++ Ritual/Assets/Resources/Scripts/Player scripts/EnemyManager.cs
@@ -1,28 +1,57 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyManager : MonoBehaviour
 {
-    public GameObject enemy;                // The enemy prefab to be spawned.
+    public GameObject[] enemies;                // The enemy prefabs to be spawned.
     public float spawnTime = 3f;            // How long between each spawn.
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-    private int enemies = 0;
+    private int spawnedEnemies = 0;
+    private List<GameObject> aliveEnemies;
+    private int aliveEnemiesTotal = 0;
+    public int maxAliveEnemies;
+    public int maxSpawnedEnemies;
 
 
     void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         InvokeRepeating("Spawn", spawnTime, spawnTime);
+        aliveEnemies = new List<GameObject>();
     }
 
+    void Update()
+    {
+        List<GameObject> deadEnemies = new List<GameObject>();
+        foreach( GameObject enemy in aliveEnemies)
+        {
+            if(enemy == null)
+            {
+                deadEnemies.Add(enemy);
+            }
+        }
+        foreach( GameObject enemy in deadEnemies)
+        {
+            aliveEnemies.Remove(enemy);
+        }
+    }
 
     void Spawn()
     {
+        if(aliveEnemies.Count < maxAliveEnemies && spawnedEnemies < maxSpawnedEnemies)
+        {
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-        // Find a random index between zero and one less than the number of spawn points.
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            // Find a random index between zero and one less than the number of enemy prefabs.
+            int enemyIndex = Random.Range(0, enemies.Length);
 
-        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, new Vector2(spawnPoints[spawnPointIndex].position.x, spawnPoints[spawnPointIndex].position.y), spawnPoints[spawnPointIndex].rotation);
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            aliveEnemies.Add( Instantiate(enemies[enemyIndex], 
+                new Vector2(spawnPoints[spawnPointIndex].position.x, spawnPoints[spawnPointIndex].position.y), 
+                spawnPoints[spawnPointIndex].rotation) as GameObject);
+            spawnedEnemies++;
+            Debug.Log(spawnedEnemies);
+        }
     }
 }
