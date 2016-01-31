29e0d40e91363564ca332ad12954849701a44a3b
PeterXhofleer <pm.xhofleer@gmail.com>
2016-01-31 15:12:59 +0100

    video aan menuscherm toegevoegd

---
+++ Ritual/Assets/Resources/Scripts/Menu scripts/VideoController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoController : MonoBehaviour {
+
+	private MovieTexture movie;
+	void Start() {
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+		movie.Play();
+		movie.loop=true;
+
+	}
+
+}
